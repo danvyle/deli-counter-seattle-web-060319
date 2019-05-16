a27525b1d353a7a743bfdcbd6ab0ebeaa0e0e4d6
Dan-Vy Le <danvyle85@gmail.com>
2019-05-16 00:21:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,8 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-  position = array.index(name)
-  puts "Welcome, #{name}. You are number #{(position)+1} in line."
+  # position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
 end 
 
 # def now_serving(array, string)
