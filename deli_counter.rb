341fee71ddcc75430f8f00b4d9777a2c196e04b6
Dan-Vy Le <danvyle85@gmail.com>
2019-05-16 00:26:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(array, string)
   puts "Welcome, #{string}. You are number #{array.index(string)+1} in line."
 end 
 
-def now_serving(array, string)
+def now_serving(array)
   array.empty?
     puts "There is nobody waiting to be served!"
   if array length > 0
